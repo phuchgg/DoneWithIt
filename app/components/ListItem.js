694b49987ff148aea6d17bb7b7aace2a3669a6a4
HoangPhuc <maximumz160@gmail.com>
2021-03-24 18:45:53 +0700

    Account Screen - V1

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -4,15 +4,16 @@ import AppText from './AppText';
 import colors from '../config/colors'
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 
-function ListItem({title, subTitle, image, onPress, renderRightActions}) {
+function ListItem({title, subTitle, image, IconComponent, onPress, renderRightActions}) {
     return (
         <Swipeable renderRightActions={renderRightActions}>
             <TouchableHighlight underlayColor={colors.lightgrey} onPress={onPress}>
                 <View style={styles.container}>
-                    <Image style={styles.image} source={image}/>
-                    <View>
+                    {IconComponent}
+                    {image && <Image style={styles.image} source={image}/>}
+                    <View style={styles.detailsContainer}>
                         <AppText style={styles.title}>{title}</AppText>
-                        <AppText style={styles.subtitle}>{subTitle}</AppText>
+                        {subTitle && <AppText style={styles.subtitle}>{subTitle}</AppText>}
                     </View>
                 </View>
             </TouchableHighlight>
@@ -23,19 +24,23 @@ function ListItem({title, subTitle, image, onPress, renderRightActions}) {
 const styles = StyleSheet.create({
     container:{
         flexDirection: "row",
-        padding: 15
+        padding: 15,
+        backgroundColor: colors.white
     },
     image:{
         width: 70,
         height: 70,
         borderRadius: 35,
-        marginRight: 10
     },
     title:{
         fontWeight: "900"
     },
     subtitle:{
         color: colors.medium
+    },
+    detailsContainer:{
+        marginLeft: 10,
+        justifyContent: 'center',
     }
 })
 
